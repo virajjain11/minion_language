1418a9f099fb664055688a2ba827f8350f2400e0
Viraj Jain <virajjain7.vj@gmail.com>
2021-12-12 07:44:13 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// alert("Everthing's good")
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector(".textInput")
+btnTranslate.addEventListener("click", function eventHandler(){
+     
+    console.log("You clicked")
+    console.log("Input is ",textInput.value )
+})
+ 
